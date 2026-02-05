a26a3d7e1648c603029505ad696618431af6deb3
esteruks016 <ekalnina1@anip.lv>
2026-02-05 10:46:45 +0200

    4455

--- script.js
+++ script.js
@@ -1,42 +1,14 @@
-var sum = 0;
-var count = 0;
+document.addEventListener("DOMContentLoaded", ()=> {
+   const dateE1 = document.getElementById("date");
+   const minE1 = document.getElementById("min");
+   const maxE1 = document.getElementById("max");
 
-function addRow() {
+   const tbody = document.querySelector("tbody");
+   const avg_summary = document.getElementById("avgAll");
+   const add_bth = document.querySelector("btn");
 
-    var dateInput = document.getElementById("date");
-    var minInput = document.getElementById("min");
-    var maxInput = document.getElementById("max");
-
-    var date = dateInput.value;
-    var min = minInput.value;
-    var max = maxInput.value;
-
-{
-        alert("Aizpildi visus laukumus");
-        return;
-    }
-
-    min = Number(min);
-    max = Number(max);
-
-    var avg = (min + max) / 2;
-
-    var table = document.getElementById("table");
-    var row = table.insertRow();
-
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    var cell4 = row.insertCell(3);
-
-    cell1.innerHTML = date;
-    cell2.innerHTML = min;
-    cell3.innerHTML = max;
-    cell4.innerHTML = avg.toFixed(2);
-
-    sum = sum + avg;
-    count = count + 1;
-
-    var avgAll = sum / count;
-    document.getElementById("avgAll").innerHTML = avgAll.toFixed(2);
-}
+   if (!dateE1 || !minE1 || !maxE1 || !tbody || !avg_summary || !add_bth) {
+       console.warn("Trūkst obligātais elements.");
+       return;
+   }
+})
